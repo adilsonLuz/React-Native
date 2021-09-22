3cf9c6d6baeb28ab5c1e2c1edd784bdc6921617d
Adilson Luz <adilson.luz@fatec.sp.gov.br>
2021-09-21 22:45:19 -0300

    clone appEcoletas

---
+++ appEcoletas/App.tsx
@@ -0,0 +1,17 @@
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Home from './src/pages/Home';
+
+export default function App() {
+  return (
+    <View>
+        <StatusBar  />
+        <Home/>
+    </View>
+  );
+}
+
+
