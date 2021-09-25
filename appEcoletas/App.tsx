32977a6ed86b8db21aa2c9ee682f9b32c733e142
Adilson Luz <adilson.luz@fatec.sp.gov.br>
2021-09-24 23:03:36 -0300

    clone appEcoletas v01

--- appEcoletas/App.tsx
+++ appEcoletas/App.tsx
@@ -1,17 +1,29 @@
-
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
+import {Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto';
+
 
 import Home from './src/pages/Home';
 
 export default function App() {
-  return (
-    <View>
-        <StatusBar  />
-        <Home/>
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  } else {
+    return(
+      <>
+      <StatusBar style='dark' backgroundColor='transparent' translucent/>
+      <Home/>
+      </>
+    );
+  }//Fecha o Else
+}// Fecha a function
 
 
